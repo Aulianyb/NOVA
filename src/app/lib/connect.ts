f7ff4e74cbe9464d51e8b0f090156264d6ca2bed
Aulianyb <18221066@std.stei.itb.ac.id>
2025-02-07 14:23:48 +0700

    init : setup nextJS and database connection

---
+++ src/app/lib/connect.ts
@@ -0,0 +1,26 @@
+import mongoose, {Connection} from "mongoose";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL undefined")
+}
+
+let cached: Connection | null = null;
+
+export async function connectToMongoDB(){
+    console.log("IM BEING CALLED!")
+    if (cached) {
+        console.log("Using cached DB connection")
+        return cached;
+    }
+    try{
+        const cnt = await mongoose.connect(DATABASE_URL!)
+        cached = cnt.connection;
+        console.log("New connection has been established");
+        return cached;
+    } catch (error){
+        console.log(error);
+        throw error;
+    }
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Register Page</h1>
+            {/* Add your login form or other components here */}
+        </div>
+    );
+};
+
+export default LoginPage;
