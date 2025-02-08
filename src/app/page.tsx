8b61c26a5c0a2030f02267d11bd13c20b0b0a642
Aulianyb <18221066@std.stei.itb.ac.id>
2025-02-08 20:38:13 +0700

    feat : landing page FE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,44 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Button } from "@mui/material";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { quantico } from "./fonts";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <main>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <h1>LANDING PAGE</h1>
-          <Button variant="contained" onClick={() => router.push("/register")}>
-            REGISTER
-          </Button>
-          <Button variant="contained" onClick={() => router.push("/login")}>
-            LOGIN
-          </Button>
+    <main className="bg-[var(--black)]">
+      <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <main className="flex flex-col gap-8 row-start-2 ml-12">
+          <div className="min-w-64">
+            <h1
+              className={`${quantico.className} text-[200px] -my-[50px] text-[var(--primary)]`}
+            >
+              NOVA
+            </h1>
+            <h2 className="text-2xl italic text-[var(--white)]">
+              Narrative Organization and Visualization Assistant
+            </h2>
+            <div className="flex justify-start gap-4 my-8">
+              <Button size="xl" onClick={() => router.push("/register")}>
+                REGISTER
+              </Button>
+              <Button
+                size="xl"
+                variant="secondary"
+                className="ml-4"
+                onClick={() => router.push("/login")}
+              >
+                LOGIN
+              </Button>
+            </div>
+          </div>
         </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
+          <h3 className="text-[var(--white)] text-xl">
+            Made with 💖 by @TheVoidMask
+          </h3>
         </footer>
       </div>
     </main>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const LoginPage: React.FC = () => {
-    return (
-        <div>
-            <h1>Register Page</h1>
-            {/* Add your login form or other components here */}
-        </div>
-    );
+  return (
+    <main className="flex justify-center items-center min-h-screen bg-[var(--black)]">
+      <div className="bg-[var(--white)] w-fit h-fit">
+        <h1>REGISTER PAGE</h1>
+        <Input placeholder="Username" />
+        <Input placeholder="Password" />
+        <Button>REGISTER</Button>
+      </div>
+    </main>
+  );
 };
 
 export default LoginPage;
