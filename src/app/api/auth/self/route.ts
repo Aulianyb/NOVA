6d6439c354dcf3fc49eba7ceec34d2f72bcb0c18
Aulianyb <18221066@std.stei.itb.ac.id>
2025-02-16 12:05:28 +0700

    fix : self API response fix for deployment

--- src/app/api/auth/self/route.ts
+++ src/app/api/auth/self/route.ts
@@ -1,14 +1,11 @@
 import { NextResponse } from "next/server";
 import { getSession } from "../session/route";
 
-export async function GET(
-  res : NextResponse
-){
+export async function GET(){
     const session = await getSession(); 
     if (!session){
-        res = NextResponse.json({message : "No session found"}, {status: 401});
+        return NextResponse.json({message : "No session found"}, {status: 401});
     } else {
-        res = NextResponse.json({message : "User Authenticated", session : session}, {status: 200}) 
+        return NextResponse.json({message : "User Authenticated", session : session}, {status: 200}) 
     }
-    return res;
 }
